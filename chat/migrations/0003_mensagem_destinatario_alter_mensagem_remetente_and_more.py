1ba351eacc93141bba0111d9ade47c408d5b6292
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-06-28 12:53:51 -0300

    Fix chat migrations and add merge migration

--- chat/migrations/0003_mensagem_destinatario_alter_mensagem_remetente_and_more.py
+++ chat/migrations/0003_mensagem_destinatario_alter_mensagem_remetente_and_more.py
@@ -34,47 +34,4 @@ class Migration(migrations.Migration):
                 to=settings.AUTH_USER_MODEL,
             ),
         ),
-        migrations.CreateModel(
-            name="Notificacao",
-            fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                (
-                    "created_at",
-                    models.DateTimeField(
-                        default=django.utils.timezone.now, editable=False
-                    ),
-                ),
-                ("updated_at", models.DateTimeField(auto_now=True)),
-                ("mensagem", models.CharField(max_length=255)),
-                ("lida", models.BooleanField(default=False)),
-                (
-                    "remetente",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="notificacoes_enviadas",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-                (
-                    "usuario",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="notificacoes",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-            ],
-            options={
-                "verbose_name": "Notificação",
-                "verbose_name_plural": "Notificações",
-            },
-        ),
     ]
