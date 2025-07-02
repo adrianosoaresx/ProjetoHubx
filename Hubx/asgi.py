4f1059ab5cd0cd32592748e2163aeeed5da4e019
SOATI <adrianosoares@soati.com.br>
2025-07-02 14:21:40 -0300

    refatorar

--- Hubx/asgi.py
+++ Hubx/asgi.py
@@ -1,21 +1,14 @@
-"""
-ASGI config for Hubx project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
+# Hubx/asgi.py
 
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Hubx.settings")
-django.setup()
+import chat.routing  # certifique-se de que existe chat/routing.py
 
-import chat.routing
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hubx.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
