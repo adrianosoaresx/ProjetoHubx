21300c6fe5f9a264d48b09d7ff0a4bdc18cf648d
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-07-03 20:04:21 -0300

    Add Nucleo detail view and member management

--- nucleos/views.py
+++ nucleos/views.py
@@ -2,8 +2,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
 from django.contrib import messages
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+    View,
+)
 from core.permissions import AdminRequiredMixin, GerenteRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Nucleo
 from .forms import NucleoForm, NucleoSearchForm
@@ -66,6 +74,11 @@ class NucleoUpdateView(GerenteRequiredMixin, LoginRequiredMixin, UpdateView):
         messages.success(self.request, "Núcleo atualizado com sucesso.")
         return super().form_valid(form)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["membros"] = self.object.membros.all()
+        return context
+
 
 class NucleoDeleteView(AdminRequiredMixin, LoginRequiredMixin, DeleteView):
     model = Nucleo
@@ -82,5 +95,32 @@ class NucleoDeleteView(AdminRequiredMixin, LoginRequiredMixin, DeleteView):
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, "Núcleo removido.")
         return super().delete(request, *args, **kwargs)
+
+
+class NucleoDetailView(GerenteRequiredMixin, LoginRequiredMixin, DetailView):
+    model = Nucleo
+    template_name = "nucleos/detail.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user = self.request.user
+        if user.tipo_id == User.Tipo.ADMIN:
+            qs = qs.filter(organizacao=user.organizacao)
+        elif user.tipo_id == User.Tipo.GERENTE:
+            qs = qs.filter(membros=user)
+        return qs
+
+
+class NucleoMemberRemoveView(GerenteRequiredMixin, LoginRequiredMixin, View):
+    def post(self, request, pk, user_id):
+        nucleo = get_object_or_404(Nucleo, pk=pk)
+        if request.user.tipo_id == User.Tipo.ADMIN and nucleo.organizacao != request.user.organizacao:
+            return redirect("nucleos:list")
+        if request.user.tipo_id == User.Tipo.GERENTE and request.user not in nucleo.membros.all():
+            return redirect("nucleos:list")
+        membro = get_object_or_404(User, pk=user_id)
+        nucleo.membros.remove(membro)
+        messages.success(request, "Membro removido do núcleo.")
+        return redirect("nucleos:update", pk=pk)
 
 
