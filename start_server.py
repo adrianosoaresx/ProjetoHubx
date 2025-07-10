ea2837537a69a7ad39a09892fe423cd7de2b3ca4
SOATI <adrianosoares@soati.com.br>
2025-07-10 10:23:13 -0300

    refatoração

--- start_server.py
+++ start_server.py
@@ -8,16 +8,16 @@ manage_path = BASE_DIR / "manage.py"
 asgi_path = "Hubx.asgi:application"
 
 def is_venv_active():
-    return (BASE_DIR / "venv" / "Scripts" / "activate").exists()
+    return (BASE_DIR / ".venv" / "Scripts" / "activate").exists()
 
 def runserver():
     print("🚀 Iniciando com Django runserver...")
-    subprocess.run(["python", str(manage_path), "runserver"])
+    subprocess.run([sys.executable, str(manage_path), "runserver"])
 
 def uvicorn_server():
     print("🌐 Iniciando com uvicorn (WebSocket ready)...")
     subprocess.run([
-        "uvicorn",
+        sys.executable, "-m", "uvicorn",
         asgi_path,
         "--reload",
         "--port", "8000"
@@ -26,8 +26,8 @@ def uvicorn_server():
 def main():
     if not is_venv_active():
         print("❗ Ambiente virtual não detectado ou não ativado. Ative com:\n")
-        print("    .\\venv\\Scripts\\activate (Windows)")
-        print("    source venv/bin/activate   (Linux/Mac)")
+        print("    .\\.venv\\Scripts\\activate (Windows)")
+        print("    source .venv/bin/activate   (Linux/Mac)")
         return
 
     print("✅ Ambiente detectado.")
