5c9e0c844755207a71bef98363c02b93b54116eb
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-07-10 15:48:34 -0300

    Restrict org app to root users

--- organizacoes/tests.py
+++ organizacoes/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Organizacao
+
+
+class OrganizacaoPermissionsTests(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.root_user = User.objects.create_user(
+            username="rootuser", password="pass", tipo_id=User.Tipo.SUPERADMIN
+        )
+        self.admin_user = User.objects.create_user(
+            username="adminuser", password="pass", tipo_id=User.Tipo.ADMIN
+        )
+        org = Organizacao.objects.create(nome="Org 1", cnpj="00.000.000/0001-00")
+        self.admin_user.organizacao = org
+        self.admin_user.save()
+
+    def test_root_can_access_list(self):
+        self.client.force_login(self.root_user)
+        response = self.client.get(reverse("organizacoes:list"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_cannot_access_list(self):
+        self.client.force_login(self.admin_user)
+        response = self.client.get(reverse("organizacoes:list"))
+        self.assertEqual(response.status_code, 403)
