5d8c032cc2d1ca6eb177efcf25bdf929c890b16b
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-07-25 21:39:10 -0300

    feat: implement email confirmation and 2fa logs

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
-from .models import NotificationSettings
+from .models import AccountToken, NotificationSettings
+from .tasks import send_confirmation_email
 
 User = get_user_model()
 
@@ -15,3 +17,10 @@ def create_notification_settings(sender, instance, created, **kwargs):
         if instance.user_type in {"root", "admin"}:
             instance.is_staff = True
             instance.save(update_fields=["is_staff"])
+        if not instance.is_active:
+            token = AccountToken.objects.create(
+                usuario=instance,
+                tipo=AccountToken.Tipo.EMAIL_CONFIRMATION,
+                expires_at=timezone.now() + timezone.timedelta(hours=24),
+            )
+            send_confirmation_email.delay(token.id)
