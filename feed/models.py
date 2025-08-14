21b7b8d80d386dc6bdc54c6ff904cdd140bda8ca
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-08-14 19:21:41 -0300

    feat(feed): add flag soft delete migration

--- feed/models.py
+++ feed/models.py
@@ -106,13 +106,16 @@ class Like(TimeStampedModel, SoftDeleteModel):
         verbose_name_plural = "Curtidas"
 
 
-class Flag(TimeStampedModel):
+class Flag(TimeStampedModel, SoftDeleteModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="flags")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="flags")
 
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
     class Meta:
-        unique_together = ("post", "user")
+        unique_together = ("post", "user", "deleted")
         verbose_name = "Denúncia"
         verbose_name_plural = "Denúncias"
 
