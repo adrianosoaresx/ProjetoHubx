66fe3bad4c85d9b909c9dab99b643d77d212b051
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-08-18 20:48:14 -0300

    test: cover organization endpoints permissions

--- organizacoes/api_urls.py
+++ organizacoes/api_urls.py
@@ -1,8 +1,40 @@
 from rest_framework.routers import DefaultRouter
 
-from .api import OrganizacaoViewSet
+from .api import (
+    OrganizacaoEmpresaViewSet,
+    OrganizacaoEventoViewSet,
+    OrganizacaoNucleoViewSet,
+    OrganizacaoPostViewSet,
+    OrganizacaoUserViewSet,
+    OrganizacaoViewSet,
+)
 
 router = DefaultRouter()
 router.register(r"organizacoes", OrganizacaoViewSet, basename="organizacao")
+router.register(
+    r"organizacoes/(?P<organizacao_pk>[^/.]+)/usuarios",
+    OrganizacaoUserViewSet,
+    basename="organizacao-usuarios",
+)
+router.register(
+    r"organizacoes/(?P<organizacao_pk>[^/.]+)/nucleos",
+    OrganizacaoNucleoViewSet,
+    basename="organizacao-nucleos",
+)
+router.register(
+    r"organizacoes/(?P<organizacao_pk>[^/.]+)/eventos",
+    OrganizacaoEventoViewSet,
+    basename="organizacao-eventos",
+)
+router.register(
+    r"organizacoes/(?P<organizacao_pk>[^/.]+)/empresas",
+    OrganizacaoEmpresaViewSet,
+    basename="organizacao-empresas",
+)
+router.register(
+    r"organizacoes/(?P<organizacao_pk>[^/.]+)/posts",
+    OrganizacaoPostViewSet,
+    basename="organizacao-posts",
+)
 
 urlpatterns = router.urls
