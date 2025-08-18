84c32186b966902434801d0c690989701468b44b
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-08-18 18:27:26 -0300

    Allow financeiro role

--- tests/urls.py
+++ tests/urls.py
@@ -2,4 +2,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path("api/notificacoes/", include("notificacoes.api_urls")),
+    path("financeiro/", include(("financeiro.urls", "financeiro"), namespace="financeiro")),
+    path(
+        "api/financeiro/",
+        include(("financeiro.api_urls", "financeiro_api"), namespace="financeiro_api"),
+    ),
 ]
