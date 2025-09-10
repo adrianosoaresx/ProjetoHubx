ad66c21c4bd0e69d0b7fed8fe7113f2ed429b8ce
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-09-10 15:40:25 -0700

    feat: add middleware to prevent caching

--- core/middleware.py
+++ core/middleware.py
@@ -1,5 +1,6 @@
 import time
 from django.utils.deprecation import MiddlewareMixin
+from django.utils.cache import add_never_cache_headers
 
 from .metrics import ENDPOINT_LATENCY
 
@@ -12,4 +13,14 @@ class EndpointMetricsMiddleware(MiddlewareMixin):
         resolver = getattr(request, "resolver_match", None)
         endpoint = resolver.view_name if resolver else request.path
         ENDPOINT_LATENCY.labels(request.method, endpoint).observe(duration)
+        return response
+
+
+class NoCacheMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        add_never_cache_headers(response)
         return response
