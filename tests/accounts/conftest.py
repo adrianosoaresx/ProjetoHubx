7ff8df81fd9c3da1fce638076c83e7a82f470b6e
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-09-29 17:53:23 -0300

    Allow admins and operators to manage user accounts

--- tests/accounts/conftest.py
+++ tests/accounts/conftest.py
@@ -4,4 +4,6 @@ import pytest
 @pytest.fixture(autouse=True)
 def celery_eager(settings, monkeypatch):
     settings.CELERY_TASK_ALWAYS_EAGER = True
+    settings.FRONTEND_URL = "http://testserver"
     monkeypatch.setattr("accounts.tasks.send_confirmation_email.delay", lambda *a, **k: None)
+    monkeypatch.setattr("accounts.tasks.send_cancel_delete_email.delay", lambda *a, **k: None)
