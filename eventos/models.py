89fbad63094760007c976400eb2032b4cfb79b42
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2025-12-15 11:44:33 -0300

    Add Evento absolute URL helper

--- eventos/models.py
+++ eventos/models.py
@@ -22,6 +22,7 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.db import models
 from django.db import transaction
+from django.urls import reverse
 from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
@@ -308,6 +309,9 @@ class Evento(TimeStampedModel, SoftDeleteModel):
 
     def __str__(self) -> str:
         return self.titulo
+
+    def get_absolute_url(self) -> str:
+        return reverse("eventos:evento_detalhe", args=[self.pk])
 
     def calcular_media_feedback(self):
         agg = self.feedbacks.aggregate(media=models.Avg("nota"))
