9305f5432f28a3eb5646f772f3b7dff200d3644e
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2026-01-29 15:03:51 -0300

    Add briefing admins and JSON widget

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.db import models
+from django_json_widget.widgets import JSONEditorWidget
 
 from .forms import EventoForm
-from .models import Evento, InscricaoEvento
+from .models import BriefingEvento, BriefingTemplate, Evento, InscricaoEvento
 
 
 @admin.register(Evento)
@@ -14,4 +16,32 @@ class EventoAdmin(admin.ModelAdmin):
 class InscricaoEventoAdmin(admin.ModelAdmin):
     list_display = ["user", "evento", "presente"]
 
+
+TEMPLATE_ALLOWED_GROUPS = ("Admin",)
+
+
+@admin.register(BriefingTemplate)
+class BriefingTemplateAdmin(admin.ModelAdmin):
+    list_display = ("nome", "ativo")
+    list_filter = ("ativo",)
+    search_fields = ("nome", "descricao")
+    formfield_overrides = {models.JSONField: {"widget": JSONEditorWidget}}
+
+    def _has_template_permission(self, request):  # pragma: no cover - admin
+        return request.user.is_superuser or request.user.groups.filter(
+            name__in=TEMPLATE_ALLOWED_GROUPS
+        ).exists()
+
+    def has_add_permission(self, request):  # pragma: no cover - admin
+        return self._has_template_permission(request)
+
+    def has_change_permission(self, request, obj=None):  # pragma: no cover - admin
+        return self._has_template_permission(request)
+
+
+@admin.register(BriefingEvento)
+class BriefingEventoAdmin(admin.ModelAdmin):
+    list_display = ("evento", "template", "status", "orcamento_enviado_em")
+    list_filter = ("status", "template__ativo")
+    search_fields = ("evento__titulo", "template__nome")
 
