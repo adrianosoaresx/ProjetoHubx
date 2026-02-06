e2c657db05a512cef994d39f597dce596dbd98f6
adrianosoaresx <159318307+adrianosoaresx@users.noreply.github.com>
2026-02-06 15:04:22 -0300

    Centraliza regra de badges para perfis institucionais

--- tests/membros/test_usuario_badges.py
+++ tests/membros/test_usuario_badges.py
@@ -6,6 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Hubx.settings")
 django.setup()
 
 from membros.templatetags import membros_extras
+from accounts.models import UserType
 
 
 class _UserStub:
@@ -98,3 +99,43 @@ def test_usuario_badges_coordenador_em_dois_nucleos_gera_cores_distintas(monkeyp
 
 def test_nucleo_style_sem_nucleo_id_faz_fallback_para_estilo_coordenador():
     assert membros_extras._nucleo_style(None) == membros_extras.BADGE_STYLES["coordenador"]
+
+
+def test_usuario_badges_admin_nao_mostra_promocao_ou_nucleo(monkeypatch):
+    user = _UserStub()
+    user.user_type = UserType.ADMIN.value
+
+    monkeypatch.setattr(
+        membros_extras,
+        "_active_participacoes_data",
+        lambda _user: [
+            {
+                "promotion_label": "Nucleado",
+                "nucleo_nome": "Núcleo A",
+                "nucleo_id": 1,
+                "is_coordenador": False,
+            }
+        ],
+    )
+
+    assert membros_extras.usuario_badges(user) == []
+
+
+def test_usuario_badges_operador_nao_mostra_promocao_ou_nucleo(monkeypatch):
+    user = _UserStub()
+    user.user_type = UserType.OPERADOR.value
+
+    monkeypatch.setattr(
+        membros_extras,
+        "_active_participacoes_data",
+        lambda _user: [
+            {
+                "promotion_label": "Coordenador",
+                "nucleo_nome": "Núcleo B",
+                "nucleo_id": 2,
+                "is_coordenador": True,
+            }
+        ],
+    )
+
+    assert membros_extras.usuario_badges(user) == []
